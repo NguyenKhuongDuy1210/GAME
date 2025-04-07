7f0df406590fe2a92d3237fdc6a6597c3082b3c3
NguyenKhuongDuy1210 <24020110@vnu.edu.vn>
2025-04-07 20:59:22 +0700

    Update Music.h

--- GAME/Music.h
+++ GAME/Music.h
@@ -15,7 +15,23 @@ public:
 
 	Mix_Chunk* loadSound(const char* path);
 	void play(Mix_Chunk* gChunk);
+
+	void loadImgVolume(SDL_Renderer* renderer);
+	void setClipsVolume();
+	int getVolume() { return volume; }
+	void renderVolume(SDL_Renderer* renderer);
+	void updateVolume(const bool is_click, int mouseX, int mouseY);
+	bool IsMouseOver(int mouseX, int mouseY);
+
 private:
+	int volume;
+	vector <SDL_Rect> frame_volume;
+	SDL_Texture* volumeTexture;
+	SDL_Rect rect_volume;
+	int width_frame;
+	int height_frame;
+	int current_frame;
+	int is_volume;
 };
 
 
